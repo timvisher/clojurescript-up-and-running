4ba5ab9d19bb75d73ad1b54a08398400163e9728
Tim Visher <tvisher@relaynetwork.com>
2013-01-22 22:06:14 -0500

    Ooo, baby! I've got me some clojurescript deps! EOM

--- hello-world/project.clj
+++ hello-world/project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojurescript "0.0-1552"]]
+  :plugins [[lein-cljsbuild "0.3.0"]]
+  :cljsbuild {:builds []})
