1bfcec24f78bf9dd10d3eafcf65aa6f418312621
Tim Visher <tvisher@relaynetwork.com>
2013-01-22 22:18:53 -0500

    Thus ends chapter 1. Hi, Charnock! EOM

--- hello-world/project.clj
+++ hello-world/project.clj
@@ -5,5 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1552"]]
+  :source-paths ["src/clj"]
   :plugins [[lein-cljsbuild "0.3.0"]]
-  :cljsbuild {:builds []})
+  :cljsbuild {:builds [{:source-path "src/cljs"
+                        :compiler {:output-to "resources/public/hello.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
