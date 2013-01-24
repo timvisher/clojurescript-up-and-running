f7dfb663e63e7f857e41adccc88b4083487d0ffa
Tim Visher <tvisher@relaynetwork.com>
2013-01-23 22:58:53 -0500

    So you use `:import` for 'classes' and `:require` for 'libraries'. Wonder what the real definition of those 2 terms is? EOM

--- keybord-events/src/keybord_events/core.cljs
+++ keybord-events/src/keybord_events/core.cljs
@@ -1,11 +1,13 @@
 (ns keybord-events.core
-  (:require [goog.events.KeyHandler]))
+  (:require [goog.events :as events])
+  (:import [goog.events KeyHandler]
+           [goog.events.KeyHandler EventType]))
 
 (defn log-event [event]
   (.log js/console event))
 
 (defn keyboard-events
   []
-  (goog.events/listen (goog.events.KeyHandler. js/document) goog.events.KeyHandler.EventType.KEY log-event))
+  (events/listen (KeyHandler. js/document) EventType.KEY log-event))
 
 (keyboard-events)
